84bd4c5df842e27b248ca5934c0f0033b60caf23
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-19 15:17:17 +0200

    used addRange method in order to add a set of elements

--- ListCollection/Program.cs
+++ ListCollection/Program.cs
@@ -9,16 +9,29 @@ namespace ListCollection
         static void Main(string[] args)
         {
             List<int> myList = new List<int>(10) { 10, 20, 30 };
-            myList.Add(1);
-            Console.WriteLine(myList.Capacity);
+
+            myList.Add(40);
             foreach (int i in myList)
             {
                 Console.WriteLine(i);
             }
-            //for (int i = 0; i < myList.Count; i++)
-            //{
-            //    Console.WriteLine(myList[i]);
-            //}
+            Console.WriteLine("-----------");
+            Console.WriteLine("capacity:" + myList.Capacity);
+            Console.WriteLine("count:" + myList.Count);
+            //Console.WriteLine(myList[4]);
+
+
+            List<int> otherList = new List<int>() { 50, 60, 70 };
+            myList.AddRange(otherList);
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            Console.WriteLine("-----------");
+            Console.WriteLine("capacity:" + myList.Capacity);
+            Console.WriteLine("count:" + myList.Count);
+
 
 
         }
