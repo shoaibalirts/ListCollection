6c28639c162efe0c7d2abbea85429901159d0534
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-19 14:47:55 +0200

    Add project files.

---
+++ ListCollection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ListCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            myList.Add(0);
+            myList.Add(1);
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
