975902d7f2ee6d4204a9eacbd5220aef07fab6ba
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-19 15:05:29 +0200

    capacity of the list class

--- ListCollection/Program.cs
+++ ListCollection/Program.cs
@@ -8,13 +8,18 @@ namespace ListCollection
     {
         static void Main(string[] args)
         {
-            List<int> myList = new List<int>() { 10, 20, 30 };
-            //myList.Add(0);
-            //myList.Add(1);
+            List<int> myList = new List<int>(10) { 10, 20, 30 };
+            myList.Add(1);
+            Console.WriteLine(myList.Capacity);
             foreach (int i in myList)
             {
                 Console.WriteLine(i);
             }
+            //for (int i = 0; i < myList.Count; i++)
+            //{
+            //    Console.WriteLine(myList[i]);
+            //}
+
 
         }
     }
