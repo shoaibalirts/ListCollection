6fe00e21e87f2d38cf4faa1022d3854c81e9f8ae
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-19 15:41:09 +0200

    Remove, RemoveAt, RemoveRange, RemoveAll, Clear methods

--- ListCollection/Program.cs
+++ ListCollection/Program.cs
@@ -31,8 +31,38 @@ namespace ListCollection
             Console.WriteLine("-----------");
             Console.WriteLine("capacity:" + myList.Capacity);
             Console.WriteLine("count:" + myList.Count);
+            Console.WriteLine("---------");
 
+            myList.InsertRange(1, otherList);
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            //int ind = 9;
+            //if (ind < myList.Count)
+            //{
+            //    myList.RemoveAt(ind);
 
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can not be removed");
+            //}
+            myList.RemoveRange(1, 3);
+            Console.WriteLine("---------");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            myList.RemoveAll(item => item < 1);
+            Console.WriteLine("-----------------");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            myList.Clear();
+            Console.WriteLine("--------");
+            Console.WriteLine("Count: " + myList.Count);
 
         }
     }
